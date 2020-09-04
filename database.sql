cc787153c9046e348ca0642726c047cc8c4a0f93
Daisy Murillo <daisy21murillo@gmail.com>
2020-09-03 22:40:59 -0700

    adding sql commands

--- database.sql
+++ database.sql
@@ -1,29 +1,30 @@
 CREATE DATABASE IF NOT EXISTS school;
 USE school;
 
-CREATE TABLE IF NOT EXISTS students (
+CREATE TABLE IF NOT EXISTS class (
+    class_id_pk INT AUTO_INCREMENT PRIMARY KEY,
+    class_name VARCHAR(100) NOT NULL,
+    teacher_name VARCHAR(100) NOT NULL,
+    num_of_laptops INT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS student (
     students_id_pk INT AUTO_INCREMENT PRIMARY KEY,
+    class_id_fk  INT NOT NULL,
     first_name VARCHAR(25) NOT NULL,
     last_name VARCHAR(25) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    phone_number INT?() NOT NULL
+    phone_number VARCHAR(10) NOT NULL,
+    FOREIGN KEY (class_id_fk) REFERENCES class (class_id_pk)
 );
 
-CREATE TABLE IF NOT EXISTS class (
-    class_id_pk INT AUTO_INCREMENT PRIMARY KEY,
-    num_of_students INT NOT NULL,
-    num_of_laptops INT NOT NULL
+CREATE TABLE IF NOT EXISTS supplies (
+    students_id_fk INT NOT NULL,
+    laptop BOOLEAN,
+    home_internet_access BOOLEAN,
+    FOREIGN KEY (students_id_fk) REFERENCES student (students_id_pk)
 );
 
-CREATE TABLE IF NOT EXISTS comments (
-    comments_id_pk INT AUTO_INCREMENT PRIMARY KEY,
-    comment VARCHAR(255) NOT NULL,
-    postings_id_fk INT,
-    users_id_fk INT,
-    FOREIGN KEY (postings_id_fk) REFERENCES postings (postings_id_pk) ON UPDATE RESTRICT ON DELETE CASCADE,
-    FOREIGN KEY (users_id_fk) REFERENCES users (users_id_pk) ON UPDATE RESTRICT ON DELETE CASCADE
-);
-
-INSERT INTO users (username, email, password) VALUES ('bob', 'bob@gmail.com', 'bobsmith123');
-INSERT INTO postings (post, users_id_fk) VALUES ('Tweeting from code!', 2);
-INSERT INTO comments (comment, postings_id_fk, users_id_fk) VALUES ('Commenting on daisys post!', 2, 2);
+-- Examples of Values
+INSERT INTO class (class_name, teacher_name, num_of_laptops) VALUES ('Math', 'Ms. Green', 25);
+INSERT INTO student (class_id_fk, first_name, last_name, email, phone_number) VALUES (3, 'Jacky', 'Mae', 'jacky.mae@email.com', '6025556789');
+INSERT INTO supplies (students_id_fk, laptop, home_internet_access) VALUES (10, true, false);
